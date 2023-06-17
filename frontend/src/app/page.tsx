19a949f364242239d9da28f0cec9803db8581f4c
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-06-17 21:58:13 +0800

    feat: better ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
-import JobTree from '@/components/JobTree';
-import Sidebar from '@/components/Sidebar';
+import CareerPathway from '@/components/CareerPathway';
+import SideNavbar from '@/components/SideNavbar';
 
 export default function Home() {
   return (
-    <main className='w-screen h-screen flex flex-row test'>
-      <Sidebar />
-      <div className='w-full h-full border border-gray-700'>
-        <JobTree />
+    <main className='w-screen h-screen flex flex-row'>
+      <SideNavbar />
+      <div className='w-full h-full p-6 flex flex-col'>
+        <CareerPathway />
       </div>
     </main>
   );
