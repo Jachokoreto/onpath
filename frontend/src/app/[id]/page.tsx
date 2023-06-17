1cb0fa8f5cc8061939f235534fe88a09eeb4e8ac
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-06-18 03:38:50 +0800

    chore: Added missing Toaster

--- frontend/src/app/[id]/page.tsx
+++ frontend/src/app/[id]/page.tsx
@@ -3,6 +3,7 @@ import CareerPathway from '@/components/CareerPathway';
 import SideNavbar from '@/components/side-navbar/SideNavbar';
 import Employee from '@/types/Employee';
 import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function Page({ params }: { params: { id: string } }) {
   const [employee, setEmployee] = useState<Employee>();
@@ -16,11 +17,12 @@ export default function Page({ params }: { params: { id: string } }) {
     };
 
     fetchData().catch(console.error);
-  }, []);
+  }, [params]);
 
   return (
     <main className='w-screen h-screen flex flex-row'>
-      {employee && <SideNavbar employee={employee} />}
+			<Toaster position='top-right' reverseOrder={false} />;
+			{employee && <SideNavbar employee={employee} />}
       <div className='w-full h-full p-6 flex flex-col'>
         <CareerPathway />
       </div>
