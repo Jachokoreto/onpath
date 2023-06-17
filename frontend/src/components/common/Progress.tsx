bc2cbdc14b91a1b034b9a246519cbfbe5d4b7622
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-06-18 00:55:42 +0800

    fix: adjust progress ui

--- frontend/src/components/common/Progress.tsx
+++ frontend/src/components/common/Progress.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 interface ProgressProps {
   progress: number;
-  bgColor: string;
+  bgColor?: string;
 }
 export default function Progress({ progress, bgColor }: ProgressProps) {
+  console.log('progress', progress);
   return (
-    <div className='w-full mt-1 bg-gray-200 rounded-full dark:bg-gray-700'>
+    <div className='w-full bg-gray-200 rounded-full dark:bg-gray-700'>
       <div
-        className={`${bgColor} text-[10px] font-medium text-blue-100 text-center p-0.5 leading-none rounded-full `}
+        className={`${
+          bgColor ? (progress > 1 ? bgColor : '') : 'bg-blue-500'
+        } text-[10px] font-medium  text-center p-0.5 leading-none rounded-full ${
+          progress > 1 ? 'text-blue-100' : 'text-slate-700'
+        } `}
         style={{ width: `${progress}%` }}
       >
         {progress}%
