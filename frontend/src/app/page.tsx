164a99079b6e367b7f7b55710ab4ca23973a1a00
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-06-17 22:53:42 +0800

    feat: a button to employer pov

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,21 @@
 'use client';
 import CareerPathway from '@/components/CareerPathway';
 import SideNavbar from '@/components/SideNavbar';
+import { Button } from 'flowbite-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='w-screen h-screen flex flex-row'>
+      <Link href={'/talentpool'}>
+        <Button
+          className='absolute top-1 right-1'
+          color={'warning'}
+          size={'xs'}
+        >
+          Employer
+        </Button>
+      </Link>
       <SideNavbar />
       <div className='w-full h-full p-6 flex flex-col'>
         <CareerPathway />
